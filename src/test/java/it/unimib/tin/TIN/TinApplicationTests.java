42ca606f7232a91d811590558acd7b3146e7a120
a.grillo22@campus.unimib.it <a.grillo22@campus.unimib.it>
2024-03-19 14:00:07 +0100

    Add spring-boot project

---
+++ src/test/java/it/unimib/tin/TIN/TinApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimib.tin.TIN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
