2ed13fa27d0ea514106d31dddce7323629c0f61e
Gabriele <g.pandini@campus.unimib.it>
2024-03-29 16:09:34 +0100

    Aggiunto controller di visualizzazione dettagli di un prodotto e pagina html dedicata. Modificata la directory di salvataggio delle immagini. (Storia 29)

---
+++ src/main/java/it/unimib/tin/TIN/controller/VisualizzaProdottoController.java
@@ -0,0 +1,37 @@
+package it.unimib.tin.TIN.controller;
+
+import it.unimib.tin.TIN.model.Prodotto;
+import it.unimib.tin.TIN.repository.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Controller
+public class VisualizzaProdottoController {
+    private ProdottoRepository prodottoRepository;
+
+    public VisualizzaProdottoController(ProdottoRepository prodottoRepository) {
+        this.prodottoRepository = prodottoRepository;
+    }
+
+    @GetMapping("/prodotto/{idProdotto}")
+    public ModelAndView infoCiclista(@PathVariable("idProdotto") Long idProdotto) {
+        ModelAndView maw = new ModelAndView();
+        Optional<Prodotto> p = this.prodottoRepository.findById(idProdotto);
+        if(p.isPresent()){
+            maw = buildInfoProdottoPage(p.get(), false);
+        }
+        return maw;
+    }
+
+    public ModelAndView buildInfoProdottoPage(Prodotto p, Boolean confirm) {
+        ModelAndView maw = new ModelAndView();
+        maw.addObject("prodotto", p);
+        maw.addObject("confirm", confirm);
+        maw.setViewName("prodottoInfo");
+        return maw;
+    }
+}
