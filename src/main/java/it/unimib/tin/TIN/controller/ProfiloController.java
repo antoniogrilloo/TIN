eb497d84c8c2a9883652bbe55cd42945f97f50ea
a.grillo22@campus.unimib.it <a.grillo22@campus.unimib.it>
2024-04-05 10:24:44 +0200

    Effettuato refactoring sul controller relativo al profilo dell'utente affinché si possano implementare i metodi per la storia (Storia 14)

--- src/main/java/it/unimib/tin/TIN/controller/ProfiloController.java
+++ src/main/java/it/unimib/tin/TIN/controller/ProfiloController.java
@@ -1,39 +1,45 @@
 package it.unimib.tin.TIN.controller;
 
-import it.unimib.tin.TIN.model.Prodotto;
+import it.unimib.tin.TIN.model.Categoria;
 import it.unimib.tin.TIN.model.UtenteAutenticato;
+import it.unimib.tin.TIN.repository.CategoriaRepository;
 import it.unimib.tin.TIN.repository.UtenteAutenticatoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
-public class VisualizzaProfiloController {
+public class ProfiloController {
 
     private UtenteAutenticatoRepository utenteAutenticatoRepository;
 
-    public VisualizzaProfiloController(UtenteAutenticatoRepository utenteAutenticatoRepository) {
+    private CategoriaRepository crepo;
+
+    public ProfiloController(UtenteAutenticatoRepository utenteAutenticatoRepository, CategoriaRepository crepo) {
         this.utenteAutenticatoRepository = utenteAutenticatoRepository;
+        this.crepo = crepo;
     }
 
     @GetMapping("/user/{user_id}")
     public ModelAndView infoUtente(@PathVariable("user_id") Long idUser) {
         ModelAndView maw = new ModelAndView();
         Optional<UtenteAutenticato> user = this.utenteAutenticatoRepository.findById(idUser);
+        String page;
         if(user.isPresent()){
-            maw = buildInfoUserPage(user.get(), false);
+            page = "userInfo";
+            List<Categoria> categories = crepo.findAll();
+            maw.addObject("categories", categories);
+            maw.addObject("user", user);
+            maw.addObject("confirm", false);
+        } else {
+            page = "error";
         }
+        maw.setViewName(page);
         return maw;
     }
 
-    public ModelAndView buildInfoUserPage(UtenteAutenticato user, Boolean confirm) {
-        ModelAndView maw = new ModelAndView();
-        maw.addObject("user", user);
-        maw.addObject("confirm", confirm);
-        maw.setViewName("userInfo");
-        return maw;
-    }
 }
