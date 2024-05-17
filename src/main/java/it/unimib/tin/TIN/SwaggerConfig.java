12e15ebd69bd0a1ef170e398549615b773b5cf23
a.grillo22@campus.unimib.it <a.grillo22@campus.unimib.it>
2024-05-17 21:53:45 +0200

    Task 9: Aggiunte impostazioni per generare doc endpoint automatiche

---
+++ src/main/java/it/unimib/tin/TIN/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package it.unimib.tin.TIN;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("TradeItNow API")
+                        .description("TIN application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("TIN Wiki Documentation")
+                        .url("https://gitlab.com/a.grillo22/tin/-/wikis/home"));
+    }
+
+}
